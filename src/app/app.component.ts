5fbed05e140b592a157c017e4780310e3518ecf7
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-02-10 15:34:00 +0000

    created arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-movie-list';
+
+  moviesList = [
+    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
+    {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
+    {id: 1, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
+    {id: 1, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'},
+  ]
 }
