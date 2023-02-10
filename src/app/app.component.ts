ad9ec56448eb71b736453c0615810853c3f085c3
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-02-10 15:51:04 +0000

    injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new-movie-list';
+ movies={};
 
-  moviesList = [
-    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
-    {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
-    {id: 1, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
-    {id: 1, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'},
-  ]
+ constructor(private movieService: MovieService) {}
+
+ ngOnInit() {
+this.movies = this.movieService.getMovies();
+console.log(this.movies);
+ }
 }
